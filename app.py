f17a69c5df034afc1d33898ed65dda0e938230c5
idy cissokho <idycissokh544@gmail.com>
2024-06-15 07:29:59 +0200

    deuxieme comite (Register, Logi)

--- app.py
+++ app.py
@@ -2,12 +2,13 @@
 from flask import Flask, render_template
 from flask_migrate import Migrate
 from config import Config
-from model import db, Plat, User, Commande, CommandePlat
-from flask import Flask, url_for, redirect, render_template
+from models import db, Plat, User, Commande, CommandePlat
+from flask import Flask, url_for, redirect, render_template, request
 from flask_migrate import Migrate
-from flask_login import LoginManager, logout_user
+from flask_login import LoginManager, logout_user, login_user, current_user, login_required
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 
@@ -15,17 +16,60 @@ app.config.from_object(Config)
 db.init_app(app)
 migrate = Migrate(app,db)
 
+login_manager = LoginManager()
+login_manager.init_app(app)
 
+
+@login_manager.user_loader
+def load_user(id): 
+    return User.get_or_404(id)
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    # form = RegisterFrame()
+    if request.method == 'POST': 
+        nom = request.form.get('nom')
+        prenom = request.form.get('prenom')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        
+        user = User.query.filter_by(email=email).first()
+        if user: 
+            redirect(url_for('login'))
+        else:
+            new_user = User(nom=nom, prenom=prenom, email=email, password=generate_password_hash(password))
+            try:
+              db.session.add(new_user)
+              db.session.commit()
+              print('insertion reussit')
+            except:
+              print('Erreur')
+              
+              
     return render_template('register.html')
 
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST': 
+        email = request.form.get('email')
+        password = request.form.get('password')
+        
+        user = User.query.filter_by(email=email).first()
+        if user and check_password_hash(user.password, password): 
+            login_user(user)
+            redirect(url_for('acceuil'))
+        else:
+            redirect(url_for('login'))
+    return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    redirect(url_for('login'))
 
 if __name__ == '__main__':
     with app.app_context():
--- config.py
+++ config.py
@@ -6,4 +6,8 @@ load_dotenv()
 class Config:
     SQLALCHEMY_DATABASE_URI =os.getenv("SQLALCHEMY_DATABASE_URI")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    FLASK_DEBUG=os.getenv("FLASK_DEBUG")
+    FLASK_APP=os.getenv("FLASK_APP")
+    FLASK_ENV=os.getenv("FLASK_ENV")
+    SECRET_KEY=os.getenv("SECRET_KEY")
 
