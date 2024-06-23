46f0cd512229dade2937ffebd85b0281e113193e
Al Hassane DIALLO <assanedialloisidk@gmail.com>
2024-06-23 03:36:03 +0000

    Modification Principal

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 import random
 from flask import Flask, json, render_template, jsonify, flash, session
 from flask_mail import Mail, Message
@@ -7,7 +6,13 @@ from config import Config
 from models import db, Plat, User, Commande, CommandePlat
 from flask import Flask, url_for, redirect, render_template, request
 from flask_migrate import Migrate
-from flask_login import LoginManager, logout_user, login_user, current_user, login_required
+from flask_login import (
+    LoginManager,
+    logout_user,
+    login_user,
+    current_user,
+    login_required,
+)
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -18,13 +23,14 @@ from flask_dance.contrib.google import make_google_blueprint, google
 from flask_dance.consumer import oauth_authorized
 
 import sys
+
 sys.setrecursionlimit(15000)
 
 app = Flask(__name__)
 
 app.config.from_object(Config)
 db.init_app(app)
-migrate = Migrate(app,db)
+migrate = Migrate(app, db)
 mail = Mail(app)
 login_manager = LoginManager()
 
@@ -32,26 +38,31 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 
 
-#fichier_json = 'myjson.json'
+# fichier_json = 'myjson.json'
 
-#with open(fichier_json, 'r') as json_file:
-    #json_data = json.load(json_file)
+# with open(fichier_json, 'r') as json_file:
+# json_data = json.load(json_file)
 
-#client_id = json_data['web']['client_id']
-#client_secret = json_data['web']['client_secret']
+# client_id = json_data['web']['client_id']
+# client_secret = json_data['web']['client_secret']
 
-# client_id = "76986610716-puqfecpsnpqm0hjcm9pvh7sj3d3chpoe.apps.googleusercontent.com"
-# client_secret = "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"
-#
-# google_bp = make_google_blueprint(
-#     client_id=client_id,
-#     client_secret=client_secret,
-#     scope=['profile', 'email', 'https://www.googleapis.com/auth/gmail.readonly', 'https://www.googleapis.com/auth/drive.readonly'],
-#     redirect_to='http://localhost:5000/google_login/authorized'
-# )
-# app.register_blueprint(google_bp, url_prefix="/google_login")
+client_id = "76986610716-puqfecpsnpqm0hjcm9pvh7sj3d3chpoe.apps.googleusercontent.com"
+client_secret = "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"
 
-#oauth = OAuth(app)
+google_bp = make_google_blueprint(
+    client_id=client_id,
+    client_secret=client_secret,
+    scope=[
+        "profile",
+        "email",
+        "https://www.googleapis.com/auth/gmail.readonly",
+        "https://www.googleapis.com/auth/drive.readonly",
+    ],
+    redirect_to="http://localhost:5000/google_login/authorized",
+)
+app.register_blueprint(google_bp, url_prefix="/google_login")
+
+# oauth = OAuth(app)
 
 
 @login_manager.user_loader
@@ -59,20 +70,23 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html', plats=plats)
+
+    return render_template("index.html", plats=plats)
+
 
 def generate_verification_code():
-    return ''.join(str(random.randint(0, 9)) for _ in range(6))
+    return "".join(str(random.randint(0, 9)) for _ in range(6))
 
-@app.route('/register', methods=['GET', 'POST'])
+
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    if request.method == 'POST': 
-        nom = request.form.get('nom')
-        prenom = request.form.get('prenom')
-        email = request.form.get('email')
-        password = request.form.get('password')
+    if request.method == "POST":
+        nom = request.form.get("nom")
+        prenom = request.form.get("prenom")
+        email = request.form.get("email")
+        password = request.form.get("password")
 
         verification_code = generate_verification_code()
         print("verification_code")
@@ -83,25 +97,29 @@ def register():
         # mail.send(msg)
 
         if not all([nom, prenom, email, password]):
-            return flash({'message': 'Vous devez remplir tous les champs'})
+            return flash({"message": "Vous devez remplir tous les champs"})
 
         user = User.query.filter_by(email=email).first()
         if user:
-            flash({'message': 'ce email a deja un compte'})
-            return redirect(url_for('login'))
+            flash({"message": "ce email a deja un compte"})
+            return redirect(url_for("login"))
         else:
-            new_user = User(nom=nom, prenom=prenom, email=email, password=generate_password_hash(password))
+            new_user = User(
+                nom=nom,
+                prenom=prenom,
+                email=email,
+                password=generate_password_hash(password),
+            )
             try:
-              db.session.add(new_user)
-              db.session.commit()
-              flash({'message': 'Votre inscription a reussit'})
-              return redirect(url_for('index'))
+                db.session.add(new_user)
+                db.session.commit()
+                flash({"message": "Votre inscription a reussit"})
+                return redirect(url_for("index"))
             except:
-              flash({'message': 'erreur'})
-              print('erreur')
+                flash({"message": "erreur"})
+                print("erreur")
 
-
-    return render_template('register.html')
+    return render_template("register.html")
 
 
 # @app.route('/verify', methods=['GET', 'POST'])
@@ -120,41 +138,39 @@ def register():
 #     return render_template('verify_email.html')
 
 
-
-
-@app.route('/login', methods=['GET', 'POST'])
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    if request.method == 'POST': 
-        email = request.form.get('email')
-        password = request.form.get('password')
-        
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+
         user = User.query.filter_by(email=email).first()
-        if user and check_password_hash(user.password, password): 
+        if user and check_password_hash(user.password, password):
             login_user(user)
-            print('reussit')
-           # flash({'message': 'Vous etes connecter'})
-            
-            return redirect(url_for('index'))
+            print("reussit")
+            # flash({'message': 'Vous etes connecter'})
+
+            return redirect(url_for("index"))
         else:
-            print('echec')
-            #flash({'message': 'email ou password incorrect'})
-            
-            return redirect(url_for('login'))
-    return render_template('modals.html')
+            print("echec")
+            # flash({'message': 'email ou password incorrect'})
+
+            return redirect(url_for("login"))
+    return render_template("modals.html")
 
 
-# @app.route('/google_login')
-# def google_login():
-#     if not google.authorized:
-#         return redirect(url_for('google.login'))
-#     resp = google.get("/oauth2/v2/userinfo")
-#     if not resp.ok:
-#         flash("Échec de la connexion Google", 'danger')
-#         return redirect(url_for('login'))
-#
-#     user_info = resp.json()
-#     email = user_info["email"]
-#     user = User.query.filter_by(email=email).first()
+@app.route("/google_login")
+def google_login():
+    if not google.authorized:
+        return redirect(url_for("google.login"))
+    resp = google.get("/oauth2/v2/userinfo")
+    if not resp.ok:
+        flash("Échec de la connexion Google", "danger")
+        return redirect(url_for("login"))
+
+    user_info = resp.json()
+    email = user_info["email"]
+    user = User.query.filter_by(email=email).first()
 
     if not user:
         user = User(email=email)
@@ -162,114 +178,112 @@ def login():
         db.session.commit()
 
     login_user(user)
-    flash('Vous êtes connecté avec Google', 'success')
-    return redirect(url_for('index'))
+    flash("Vous êtes connecté avec Google", "success")
+    return redirect(url_for("index"))
 
-# @app.route('/google_login/authorized')
-# def google_authorized():
-#     resp = google.authorized_response()
-#     if resp is None or resp.get('access_token') is None:
-#         return "Access denied: reason={} error={}".format(
-#             request.args.get('error_reason'),
-#             request.args.get('error_description')
-#         )
-#     session['google_token'] = (resp['access_token'], '')
-#     user_info = google.get('userinfo')
-#     return user_info.json()
 
-@app.route('/logout')
+@app.route("/google_login/authorized")
+def google_authorized():
+    resp = google.authorized_response()
+    if resp is None or resp.get("access_token") is None:
+        return "Access denied: reason={} error={}".format(
+            request.args.get("error_reason"), request.args.get("error_description")
+        )
+    session["google_token"] = (resp["access_token"], "")
+    user_info = google.get("userinfo")
+    return user_info.json()
+
+
+@app.route("/logout")
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('index'))
+    return redirect(url_for("index"))
 
-# @oauth_authorized.connect_via(google_bp)
-# def google_logged_in(blueprint, token):
-#     resp = blueprint.session.get("/oauth2/v2/userinfo")
-#     if resp.ok:
-#         user_info = resp.json()
-#         email = user_info["email"]
-#         user = User.query.filter_by(email=email).first()
-#         if not user:
-#             user = User(email=email)
-#             db.session.add(user)
-#             db.session.commit()
-#         login_user(user)
 
-@app.route('/add_to_cart', methods=['POST'])
+@oauth_authorized.connect_via(google_bp)
+def google_logged_in(blueprint, token):
+    resp = blueprint.session.get("/oauth2/v2/userinfo")
+    if resp.ok:
+        user_info = resp.json()
+        email = user_info["email"]
+        user = User.query.filter_by(email=email).first()
+        if not user:
+            user = User(email=email)
+            db.session.add(user)
+            db.session.commit()
+        login_user(user)
+
+
+@app.route("/add_to_cart", methods=["POST"])
 def add_to_cart():
     plat = request.form.to_dict()
-    cart = session.get('cart', [])
+    cart = session.get("cart", [])
     cart.append(plat)
-    session['cart'] = cart
-    session['cart_count'] = len(cart)
-    return jsonify({'count': session['cart_count']})
+    session["cart"] = cart
+    session["cart_count"] = len(cart)
+    return jsonify({"count": session["cart_count"]})
 
-@app.route('/cart', methods=['POST','GET'])
+
+@app.route("/cart", methods=["POST", "GET"])
 def cart():
-    if request.method == 'POST': 
-        if current_user.is_authenticated: 
-            flash({'message': f'Merci {nom} {prenom} pour votre commande'})
-            print('Compte existe')
+    if request.method == "POST":
+        if current_user.is_authenticated:
+            flash({"message": f"Merci  pour votre commande"})
+            print("Compte existe")
         else:
-            nom = request.form.get('nom')
-            prenom = request.form.get('prenom')
-            email = request.form.get('email')
-            password = request.form.get('password')
-            new_user = User(nom=nom, prenom=prenom, email=email, password=generate_password_hash(password))
+            nom = request.form.get("nom")
+            prenom = request.form.get("prenom")
+            email = request.form.get("email")
+            password = request.form.get("password")
+            new_user = User(
+                nom=nom,
+                prenom=prenom,
+                email=email,
+                password=generate_password_hash(password),
+            )
             db.session.add(new_user)
             db.session.commit()
-            flash({'message': f'Merci {nom} {prenom} Compte a ete creer'})
-            print('Compte a ete creer')
-    cart = session.get('cart', [])
-    return render_template('cart.html', cart=cart)
+            flash({"message": f"Merci Compte a ete creer"})
+            print("Compte a ete creer")
+    cart = session.get("cart", [])
+    return render_template("cart.html", cart=cart)
 
-@app.route('/contact')
+
+@app.route("/contact")
 def contact():
-    return render_template('contact.html')
+    return render_template("contact.html")
 
-@app.route('/blog')
+
+@app.route("/blog")
 def blog():
-    return render_template('blog.html')
+    return render_template("blog.html")
 
-@app.route('/boutique')
+
+@app.route("/boutique")
 def boutique():
-<<<<<<< HEAD
-    return render_template('boutique.html')
-=======
-    
-    return render_template('boutique.html', plats=plats)
->>>>>>> origin/master
+
+    return render_template("boutique.html", plats=plats)
 
 
-@app.route('/create_commande', methods=['GET', 'POST'])
+@app.route("/create_commande", methods=["GET", "POST"])
 # @login_required
 def create_commande():
-    if request.method == ' POST': 
+    if request.method == " POST":
         user_id = current_user.id
         date_commande = datetime.utcnow()
         statut = "En Cours"
         commande = Commande(user_id=user_id, date_commande=date_commande, statut=statut)
-        selectplats = request.form.getlist('nomselectplat')
         db.session.add(commande)
         db.session.commit()
-        disponible_plats = Plat.query.filter_by(disponibilite = True).all()#get plat dispo
-        plats_dict = {plat.id: plat for plat in disponible_plats}#chaque plat sur une liste
 
-    else: 
-        flash('message', 'erreur')
-        
-        
-    return render_template(url_for('create_commande.html'))
+    else:
+        flash("message", "erreur")
+
+    return render_template(url_for("create_commande.html"))
 
 
-
-@app.route('/blog')
-def blog():
-    return render_template('blog.html')
-
-
-if __name__ == '__main__':
+if __name__ == "_main_":
     with app.app_context():
         try:
             # Créer toutes les tables définies dans les modèles
@@ -277,4 +291,4 @@ if __name__ == '__main__':
             print("Connexion réussie à la base de données !")
         except Exception as e:
             print("Erreur lors de la connexion à la base de données :", e)
-    app.run(debug=True)  
+    app.run(debug=True)
--- data.py
+++ data.py
@@ -2,7 +2,6 @@
 
 plats = [
     {
-<<<<<<< HEAD
         "nom": "Plat 2",
         "description": "Description du Plat 2",
         "image": "image/prend1.jpg",
@@ -49,53 +48,51 @@ plats = [
         "description": "Description du Plat 3",
         "image": "image/plat6.jpg",
         "prix": 8.99,
-=======
-        'nom': 'Plat 2',
-        'description': 'Description du Plat 2',
-        'image': 'image/prend1.jpg',
-        'prix': 12.99
+        "nom": "Plat 2",
+        "description": "Description du Plat 2",
+        "image": "image/prend1.jpg",
+        "prix": 12.99,
     },
     {
-        'nom': 'Plat 2',
-        'description': 'Description du Plat 2',
-        'image': 'image/prend2.jpg',
-        'prix': 12.99
+        "nom": "Plat 2",
+        "description": "Description du Plat 2",
+        "image": "image/prend2.jpg",
+        "prix": 12.99,
     },
     {
-        'nom': 'Plat 2',
-        'description': 'Description du Plat 2',
-        'image': 'image/prend3.jpg',
-        'prix': 12.99
+        "nom": "Plat 2",
+        "description": "Description du Plat 2",
+        "image": "image/prend3.jpg",
+        "prix": 12.99,
     },
     {
-        'nom': 'Plat 1',
-        'description': 'Description du Plat 1',
-        'image': 'image/prend4.jpg', 
-        'prix': 10.99
+        "nom": "Plat 1",
+        "description": "Description du Plat 1",
+        "image": "image/prend4.jpg",
+        "prix": 10.99,
     },
     {
-        'nom': 'Plat 2',
-        'description': 'Description du Plat 2',
-        'image': 'image/prend5.jpg',
-        'prix': 12.99
+        "nom": "Plat 2",
+        "description": "Description du Plat 2",
+        "image": "image/prend5.jpg",
+        "prix": 12.99,
     },
     {
-        'nom': 'Plat 3',
-        'description': 'Description du Plat 3',
-        'image': 'image/prend6.jpg',
-        'prix': 8.99
+        "nom": "Plat 3",
+        "description": "Description du Plat 3",
+        "image": "image/prend6.jpg",
+        "prix": 8.99,
     },
     {
-        'nom': 'Plat 3',
-        'description': 'Description du Plat 3',
-        'image': 'image/prend7.jpg',
-        'prix': 8.99
+        "nom": "Plat 3",
+        "description": "Description du Plat 3",
+        "image": "image/prend7.jpg",
+        "prix": 8.99,
     },
-     {
-        'nom': 'Plat 3',
-        'description': 'Description du Plat 3',
-        'image': 'image/plat6.jpg',
-        'prix': 8.99
->>>>>>> origin/master
+    {
+        "nom": "Plat 3",
+        "description": "Description du Plat 3",
+        "image": "image/plat6.jpg",
+        "prix": 8.99,
     },
 ]
